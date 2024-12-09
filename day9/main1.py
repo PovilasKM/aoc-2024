a1d5d64b2c0f7d81aff1d74995bc10cecad921a6
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2024-12-09 14:19:21 +0200

    way too many extra steps to achieve the same...

--- day9/main1.py
+++ day9/main1.py
@@ -1,5 +1,72 @@
-lines = open('data1.txt', "r").read().split("\n")
+def find_first_empty_index(symbols):
+  for i in range(0, len(symbols)):
+    if symbols[i][1] == -1 and symbols[i][0] > 0:
+      return i
+  return -1
+
+
+line = open('data.txt', "r").read()[:-1]
 total = 0
-for line in [x for x in lines if x]:
-  print(line)
+symbols = []
+id = 0
+is_space = False
+for symbol in line:
+  if is_space:
+    symbols.append((int(symbol), -1))
+  else:
+    symbols.append((int(symbol), id))
+    id += 1
+  is_space = not is_space
+# print(symbols)
+
+no_space = False
+i = len(symbols)-1
+while i >= 0:
+  if symbols[i][1] == -1:
+    i -= 1
+    continue
+  while symbols[i][0] > 0:
+    file_id = symbols[i][1]
+    free_index = find_first_empty_index(symbols)
+    if free_index == -1 or free_index > i:
+      no_space = True
+      break
+    symbols[i] = (symbols[i][0] - 1, file_id)
+    # this file is already here
+    # if symbols[free_index-1][1] == file_id:
+    #   symbols[free_index - 1] = (symbols[free_index-1][0] + 1, file_id)
+    #   symbols[free_index] = (symbols[free_index][0] - 1, symbols[free_index][1])
+    # # file is not already here
+    # else:
+    symbols[free_index] = (symbols[free_index][0] - 1, -1)
+    symbols.insert(free_index, (1, file_id))
+    i += 1
+  i -= 1
+  if no_space:
+    break
+
+# remove non-existing space
+symbols = [x for x in symbols if x[1] != -1 and x[0] != 0]
+
+# print(symbols)
+# join same numbers going in a row
+i = 0
+target = len(symbols) -1
+while i < target:
+  if symbols[i][1] == symbols[i+1][1]:
+    symbols[i] = (symbols[i][0] + symbols[i+1][0], symbols[i][1])
+    symbols.pop(i+1)
+    target -= 1
+  i += 1
+
+# print(symbols)
+total = 0
+i = 0
+multiplier = 0
+while i < len(symbols):
+  for j in range(0, symbols[i][0]):
+    total += multiplier * symbols[i][1]
+    multiplier += 1
+  i += 1
+
 print(total)
--- day9/main2.py
+++ day9/main2.py
@@ -1,5 +1,63 @@
-lines = open('data2.txt', "r").read().split("\n")
+def find_first_empty_index(symbols, size):
+  for i in range(0, len(symbols)):
+    if symbols[i][1] == -1 and symbols[i][0] >= size:
+      return i
+  return -1
+
+
+line = open('data2.txt', "r").read()[:-1]
 total = 0
-for line in [x for x in lines if x]:
-  print(line)
+symbols = []
+id = 0
+is_space = False
+for symbol in line:
+  if is_space:
+    symbols.append((int(symbol), -1))
+  else:
+    symbols.append((int(symbol), id, False))
+    id += 1
+  is_space = not is_space
+# print(symbols)
+
+i = len(symbols)-1
+while i >= 0:
+  if symbols[i][1] == -1 or symbols[i][2]:
+    i -= 1
+    continue
+  file_id = symbols[i][1]
+  block_size = symbols[i][0]
+  free_index = find_first_empty_index(symbols, block_size)
+  if free_index == -1 or free_index > i:
+    i -=1
+    continue
+  symbols[i] = (block_size, -1)
+  symbols[free_index] = (symbols[free_index][0] - block_size, -1)
+  symbols.insert(free_index, (block_size, file_id, True))
+
+# print(symbols)
+# remove non-existing space
+symbols = [x for x in symbols if x[0] != 0]
+
+# print(symbols)
+# join same numbers going in a row
+i = 0
+target = len(symbols) -1
+while i < target:
+  if symbols[i][1] == symbols[i+1][1]:
+    symbols[i] = (symbols[i][0] + symbols[i+1][0], symbols[i][1])
+    symbols.pop(i+1)
+    target -= 1
+  i += 1
+
+# print(symbols)
+total = 0
+i = 0
+multiplier = 0
+while i < len(symbols):
+  for j in range(0, symbols[i][0]):
+    if symbols[i][1] != -1:
+      total += multiplier * symbols[i][1]
+    multiplier += 1
+  i += 1
+
 print(total)
